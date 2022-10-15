b33c56e3b9b5ff593c6d89fde0177ffa298996cd
Andressa Novaes <isa.desenvolvimento@gmail.com>
2022-10-15 14:47:02 -0300

    feat: implementa monorepo procfiles

--- packages/web/src/server/index.ts
+++ packages/web/src/server/index.ts
@@ -7,7 +7,7 @@ export interface ResponseSuccessProps {
 }
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'localhost:3333', //process.env.REACT_APP_API_URL,
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json'
